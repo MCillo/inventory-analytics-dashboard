9ec6fe56939249d892634e5c9d82c9c79b2c23bc
Michael Cillo <michael.cillo@me.com>
2026-07-01 09:04:09 -0400

    Update test_queries.py
    Added query functions for:
    Show Tables
    Show Import Batch Count
    Show Latest Import Batch ID
    Show Product Count
    Added Aliases to Show Latest Inventory Snapshot query
    Updated Get Sales History query display

--- src/database/test_queries.py
+++ src/database/test_queries.py
@@ -30,6 +30,71 @@ def create_connection():
         print(f"Database connection error: {error}")
         raise
 
+
+# Function to show All the tables in the Database
+def show_tables(connection):
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT name 
+        FROM sqlite_master
+        WHERE type = 'table'
+        ORDER BY name;
+    """)
+
+    results = cursor.fetchall()
+
+    print("\nTables in Database")
+    for row in results:
+        print(row)
+
+# Function to show the Import Batch Count
+def show_import_batch_count(connection):
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM ImportBatch
+    """)
+
+    results = cursor.fetchone()
+
+    print("\nBatch Count")
+    print(results)
+
+# Function to show the latest Import Batch Id
+def show_latest_import_batch_id(connection):
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT 
+            ImportBatchID,
+            ImportType,
+            Status
+        FROM ImportBatch
+        ORDER BY ImportBatchID DESC
+        LIMIT 1;
+    """)
+
+    results = cursor.fetchone()
+
+    print("\nLatest Import Batch")
+    print(results)
+
+# Function to show Product Count
+def show_product_count(connection):
+    cursor = connection.cursor()
+        
+    cursor.execute("""
+        SELECT COUNT (*)
+        FROM Product
+    """)
+
+    results = cursor.fetchone()
+    product_count = results[0]
+    print("\nTotal Number of Products:")
+    print(product_count)
+
 # Function to show each product with it qty on hand
 def show_product_quantity(connection):
     cursor = connection.cursor()
@@ -49,8 +114,7 @@ def show_product_quantity(connection):
     results = cursor.fetchall()
 
     print("\nProduct Quantities:")
-    for row in results:
-        print(row)
+    print(results)
 
 # Function to show latest inventory snapshot
 def show_latest_inventory_snapshot(connection):
@@ -61,10 +125,10 @@ def show_latest_inventory_snapshot(connection):
             Product.SKU,
             Product.ProductName,
             InventorySnapshot.QuantityOnHand,
-            ROUND(ProductFinancial.CostPrice, 2),
-            ROUND(ProductFinancial.RetailPrice, 2),
-            ROUND(ProductFinancial.MarginPercent, 2),
-            ROUND(ProductFinancial.MarginDollars, 2)
+            ROUND(ProductFinancial.CostPrice, 2) AS CostPrice,
+            ROUND(ProductFinancial.RetailPrice, 2) AS RetailPrice,
+            ROUND(ProductFinancial.MarginPercent, 2) AS MarginPercent,
+            ROUND(ProductFinancial.MarginDollars, 2) AS MarginDollars
         FROM Product
         JOIN InventorySnapshot
             ON Product.ProductId = InventorySnapshot.ProductId
@@ -86,9 +150,33 @@ def show_latest_inventory_snapshot(connection):
     results = cursor.fetchall()
 
     print("\nLatest Inventory Snapshot:")
-    for row in results:
-        print(row)
 
+    print(
+        f"{'SKU':<10} "
+        f"{'ProductName':<40} "
+        f"{'QTY':>8} "
+        f"{'Cost':>10} "
+        f"{'Retail':>10} "
+        f"{'Margin %':>10} "
+        f"{'Margin $':>10}"
+    )
+
+    print("-" * 105)
+
+    for row in results:
+        sku, product_name, quantity_on_hand, cost_price, retail_price, margin_percent, margin_dollars = row
+
+        print(
+            f"{sku:<10} "
+            f"{product_name:<40} "
+            f"{quantity_on_hand:>8} "
+            f"{cost_price:>10.2f} "
+            f"{retail_price:>10.2f} "
+            f"{margin_percent:>10.2f} "
+            f"{margin_dollars:>10.2f}"
+        )
+
+# Function to Get Sales History
 def get_sales_history(connection):
     cursor = connection.cursor()
 
@@ -126,22 +214,26 @@ def get_sales_history(connection):
     results = cursor.fetchall()
 
     print("\nLatest Estimated Weeks on Hand:")
-    print(f"{'SKU':<10} {'ProductName':<35} {'QtyOnHand':<10} {'UnitsSold':<10} {'EstWOH':<10}")
+    print(f"{'SKU':<10} {'ProductName':<35} {'QtyOnHand':>10} {'UnitsSold':>10} {'EstWOH':>10}")
     print("-" * 80)
 
     for row in results:
         sku, product_name, qty_on_hand, units_sold, estimated_woh = row
 
-        print(f"{sku:<10} {product_name:<35} {qty_on_hand:<10} {units_sold:<10} {estimated_woh:<10}")
+        print(f"{sku:<10} {product_name:<35} {qty_on_hand:>10} {units_sold:>10} {estimated_woh:>10}")
 
 # Main Function
 def main():
     
     connection = create_connection()
 
-    show_product_quantity(connection)
-    show_latest_inventory_snapshot(connection)
-    get_sales_history(connection)
+    #show_tables(connection)
+    #show_import_batch_count(connection)
+    #show_latest_import_batch_id(connection)
+    show_product_count(connection)
+    #show_product_quantity(connection)
+    #show_latest_inventory_snapshot(connection)
+    #get_sales_history(connection)
 
     connection.close()
 
